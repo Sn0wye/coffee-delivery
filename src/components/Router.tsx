a2cab5b27630e2c324b1b0eb6459f7b9146733a1
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-13 13:42:21 -0300

    feat: add base layout

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,10 +1,13 @@
 import { Route } from 'wouter';
+import { DefaultLayout } from '../layouts/DefaultLayout';
+import { Checkout } from '../pages/Checkout';
 import { Home } from '../pages/Home';
 
 export const Router = () => {
   return (
-    <>
+    <DefaultLayout>
       <Route path='/' component={Home} />
-    </>
+      <Route path='/checkout' component={Checkout} />
+    </DefaultLayout>
   );
 };
---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+import { Header } from '../components/Header';
+
+export const DefaultLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
