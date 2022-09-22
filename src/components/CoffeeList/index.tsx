fcfcf540d346d875c7bc54031508c16de164bd4e
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-22 11:44:12 -0300

    fix: pass props correctly

--- src/components/CoffeeList/index.tsx
+++ src/components/CoffeeList/index.tsx
@@ -1,14 +1,16 @@
 import { coffeeOptions } from '../../data/coffee';
+import { useCart } from '../../hooks/useCart';
 import { Card } from '../Card';
 import { CoffeeListContainer, List } from './styles';
 
 export const CoffeeList = () => {
+  const state = useCart();
   return (
     <CoffeeListContainer>
       <h2>Nossos cafés</h2>
       <List>
         {coffeeOptions.map((coffee) => (
-          <Card key={coffee.id} {...coffee} />
+          <Card key={coffee.id} coffee={coffee} />
         ))}
       </List>
     </CoffeeListContainer>
