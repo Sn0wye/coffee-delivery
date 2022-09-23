64182d226537e5ca7fcf99b0b3c532b7cad13cdc
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-23 13:35:14 -0300

    refactor: mount checkout section

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const CheckoutContainer = styled.div`
   max-width: 1156px;
@@ -16,71 +16,6 @@ export const CheckoutContainer = styled.div`
     font-family: 'Baloo 2';
     font-size: 1.125rem;
   }
-`;
-
-export const Input = styled.input`
-  width: 100%;
-  font-size: 0.875rem;
-  background: ${(props) => props.theme.base.input};
-  color: ${(props) => props.theme.base.text};
-  padding: 0.75rem;
-  border-radius: 4px;
-  border: 1px solid transparent;
-
-  &::placeholder {
-    color: ${(props) => props.theme.base.label};
-  }
-
-  &:focus {
-    border: 1px solid ${(props) => props.theme['yellow-dark']};
-  }
-`;
-
-export const AddressForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
-
-export const FormGroup = styled.div`
-  display: grid;
-  grid-template-columns: 12.5rem 1fr 3.75rem;
-  gap: 0.75rem;
-`;
-
-export const PaymentOptions = styled.div`
-  display: flex;
-  gap: 0.75rem;
-`;
-
-interface RadioGroupProps {
-  isActive: boolean;
-}
-
-export const RadioGroup = styled.button<RadioGroupProps>`
-  padding: 1rem;
-  text-transform: uppercase;
-  color: ${(props) => props.theme.base.text};
-  background: ${(props) => props.theme.base.button};
-  font-size: 0.75rem;
-  line-height: 1.6;
-  display: flex;
-  gap: 0.75rem;
-  border-radius: 6px;
-  transition: background-color 0.2s ease-in-out;
-  cursor: pointer;
-  border: 1px solid transparent;
-
-  &:hover {
-    background: ${(props) => props.theme.base.hover};
-  }
-
-  ${({ isActive }) =>
-    isActive &&
-    css`
-      border: 1px solid ${(props) => props.theme.purple};
-      background: ${(props) => props.theme['purple-light']};
-    `}
 `;
 
 export const ConfirmOrderButton = styled.button`
