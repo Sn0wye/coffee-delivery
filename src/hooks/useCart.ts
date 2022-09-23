5c5b1649144010b3ab97fad659567bb3cf420cce
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-23 11:55:15 -0300

    feat: finish useCart hook

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -9,6 +9,10 @@ interface ContextType {
   products: Product[];
   addProduct: (product: Product) => void;
   removeProduct: (productId: Product['id']) => void;
+  changeQuantityById: (
+    productId: Product['id'],
+    action: 'increase' | 'decrease'
+  ) => void;
   total: {
     items: number;
     value: number;
@@ -29,7 +33,7 @@ export const useCart = create<ContextType>((set, get) => ({
         (p) => p.id !== product.id
       );
 
-      set((state) => ({
+      set(() => ({
         products: [
           ...arrayWithoutProductToBeAdded,
           {
@@ -37,6 +41,9 @@ export const useCart = create<ContextType>((set, get) => ({
             quantity: previousProduct.quantity + product.quantity,
           },
         ],
+      }));
+
+      set((state) => ({
         total: state.products.reduce(
           (total, acc) => {
             return {
@@ -55,6 +62,9 @@ export const useCart = create<ContextType>((set, get) => ({
 
     set((state) => ({
       products: [...state.products, product],
+    }));
+
+    set((state) => ({
       total: state.products.reduce(
         (total, acc) => {
           return {
@@ -73,8 +83,11 @@ export const useCart = create<ContextType>((set, get) => ({
     const newProductsArray = get().products.filter(
       (product) => product.id !== productId
     );
-    set((state) => ({
+    set(() => ({
       products: newProductsArray,
+    }));
+
+    set((state) => ({
       total: state.products.reduce(
         (total, acc) => {
           return {
@@ -88,5 +101,51 @@ export const useCart = create<ContextType>((set, get) => ({
         }
       ),
     }));
+  },
+
+  changeQuantityById: (productId, action) => {
+    let operation: number;
+
+    if (action === 'increase') {
+      operation = 1;
+    }
+    if (action === 'decrease') {
+      operation = -1;
+    }
+
+    const product = get().products.find((p) => p.id === productId);
+
+    if (product && action === 'decrease' && product.quantity === 1) return;
+
+    const arrayWithoutProductToBeUpdated: Product[] = get().products.filter(
+      (p) => p.id !== productId
+    );
+
+    if (product) {
+      set((state) => ({
+        products: [
+          ...arrayWithoutProductToBeUpdated,
+          {
+            ...product,
+            quantity: product.quantity + operation,
+          },
+        ],
+      }));
+
+      set((state) => ({
+        total: state.products.reduce(
+          (total, acc) => {
+            return {
+              items: acc.quantity + total.items,
+              value: acc.price * acc.quantity + total.value,
+            };
+          },
+          {
+            items: 0,
+            value: 0,
+          }
+        ),
+      }));
+    }
   },
 }));
