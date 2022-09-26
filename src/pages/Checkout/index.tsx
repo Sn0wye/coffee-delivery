0a8fb4b68dd85240d6777864f585e95be4b1b95f
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-26 18:16:48 -0300

    feat: add form and form provider

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,23 +1,41 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormProvider, useForm } from 'react-hook-form';
 import { AddressForm } from '../../components/AddressForm';
 import { OrderSummary } from '../../components/OrderSummary';
 import { Payment } from '../../components/Payment';
+import { AddressFormData, addressFormSchema } from '../../schemas/addressForm';
 import { CheckoutContainer } from './styles';
 
 export const Checkout = () => {
+  const addressForm = useForm<AddressFormData>({
+    resolver: zodResolver(addressFormSchema),
+    defaultValues: {
+      city: '',
+      compliment: '',
+      neighborhood: '',
+      number: undefined,
+      state: '',
+      street: '',
+      zip: 0,
+    },
+  });
+
   return (
     <CheckoutContainer>
-      <div>
-        <h3>Complete seu pedido</h3>
-        <AddressForm />
+      <FormProvider {...addressForm}>
+        <div>
+          <h3>Complete seu pedido</h3>
+          <AddressForm />
 
-        <Payment />
-      </div>
+          <Payment />
+        </div>
 
-      <div>
-        <h3>Cafés selecionados</h3>
+        <div>
+          <h3>Cafés selecionados</h3>
 
-        <OrderSummary />
-      </div>
+          <OrderSummary />
+        </div>
+      </FormProvider>
     </CheckoutContainer>
   );
 };
