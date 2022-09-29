91450f09ffd7d0fa49b5907ce55385d7e7be01e9
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-29 11:26:43 -0300

    feat: add toast when coffee added to cart

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,6 @@
 import { Minus, Plus, ShoppingCart } from 'phosphor-react';
 import { useState } from 'react';
+import { toast, Toaster } from 'react-hot-toast';
 
 import { Coffee } from '../../data/coffee';
 import { useCart } from '../../hooks/useCart';
@@ -28,6 +29,10 @@ export const Card = ({ coffee }: Props) => {
       ...coffee,
       quantity
     });
+
+    toast.success('Café adicionado ao carrinho!', {
+      position: 'bottom-right'
+    });
   };
 
   const handleIncreaseQuantity = () => {
@@ -40,35 +45,42 @@ export const Card = ({ coffee }: Props) => {
   };
 
   return (
-    <CardContainer>
-      <img src={image} alt={`Image of a ${name}`} />
-      <Tags>
-        {tags.map(tag => (
-          <Tag key={tag}>{tag}</Tag>
-        ))}
-      </Tags>
-      <h3>{name}</h3>
-      <p>{description}</p>
-
-      <Details>
-        <Price>
-          R$ <span>{price.toFixed(2)}</span>
-        </Price>
-        <Actions>
-          <CounterSelect>
-            <button onClick={handleDecreaseQuantity}>
-              <Minus size={20} color='#8047F8' weight='fill' />
-            </button>
-            {quantity}
-            <button onClick={handleIncreaseQuantity}>
-              <Plus size={20} color='#8047F8' weight='fill' />
-            </button>
-          </CounterSelect>
-          <CartButton onClick={handleAddProductToCart}>
-            <ShoppingCart size={22} weight='fill' />
-          </CartButton>
-        </Actions>
-      </Details>
-    </CardContainer>
+    <>
+      <CardContainer>
+        <img src={image} alt={`Imagem de um ${name}`} />
+        <Tags>
+          {tags.map(tag => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </Tags>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <Details>
+          <Price>
+            R$ <span>{price.toFixed(2)}</span>
+          </Price>
+          <Actions>
+            <CounterSelect>
+              <button onClick={handleDecreaseQuantity}>
+                <Minus size={20} color='#8047F8' weight='fill' />
+              </button>
+              {quantity}
+              <button onClick={handleIncreaseQuantity}>
+                <Plus size={20} color='#8047F8' weight='fill' />
+              </button>
+            </CounterSelect>
+            <CartButton onClick={handleAddProductToCart}>
+              <ShoppingCart size={22} weight='fill' />
+            </CartButton>
+          </Actions>
+        </Details>
+      </CardContainer>
+      <Toaster
+        position='bottom-center'
+        toastOptions={{
+          style: { boxShadow: '0 1px 2px 0 rgb(0 0 0 / 0.05)' }
+        }}
+      />
+    </>
   );
 };
